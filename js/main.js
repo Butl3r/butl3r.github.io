1b468dda6a3081c24b43b295668e2e276e53022d
butl3r <butl3r@localhost.localdomain>
2020-08-12 11:09:04 +1000

    Table of Contents for Desktop

--- js/main.js
+++ js/main.js
@@ -3,3 +3,15 @@ SimpleJekyllSearch({
     resultsContainer: document.getElementById('results-container'),
     json: '/search.json'
 })
+
+tocbot.init({
+    // Where to render the table of contents.
+    tocSelector: '.js-toc',
+    // Where to grab the headings to build the table of contents.
+    contentSelector: '.js-toc-content',
+    // Which headings to grab inside of the contentSelector element.
+    headingSelector: 'h1, h2',
+    // For headings inside relative or absolute positioned containers within content.
+    hasInnerContainers: true,
+    headingsOffset :  1
+});
