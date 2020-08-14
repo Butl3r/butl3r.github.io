b92dbeb5a3278963492f190d41e60044c6ca429b
butl3r <butl3r@localhost.localdomain>
2020-08-14 11:16:45 +1000

    Copy commands

--- js/main.js
+++ js/main.js
@@ -16,3 +16,25 @@ tocbot.init({
     headingsOffset :  1,
     scrollSmooth: true
 });
+
+let codes = document.querySelectorAll('.highlight > pre > code');
+let countID = 0;
+codes.forEach((code) => {
+
+  code.setAttribute("id", "code" + countID);
+  
+  let btn = document.createElement('button');
+  btn.innerHTML = "";
+  btn.className = "btn-copy far fa-copy";
+  btn.setAttribute("data-clipboard-action", "copy");
+  btn.setAttribute("data-clipboard-target", "#code" + countID);
+  
+  let div = document.createElement('div');
+  div.appendChild(btn);
+  
+  code.before(div);
+
+  countID++;
+}); 
+
+let clipboard = new ClipboardJS('.btn-copy');
